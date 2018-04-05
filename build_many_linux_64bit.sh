78d51b1cf065cd9f8e0ca1a548ccf55bc9b9235f
Laurent Mazuel <laurent.mazuel@gmail.com>
2018-04-04 17:03:43 -0700

    Skip building samples

--- build_many_linux_64bit.sh
+++ build_many_linux_64bit.sh
@@ -4,12 +4,12 @@ set -e
 # To execute this script:
 # docker run --rm -v $PWD:/data pyca/cryptography-manylinux1:x86_64 /data/build_many_linux_64bit.sh
 
+# This container is shiped with cmake version 2.8.11.2 pre-installed
+
 export UAMQP_VERSION="0.1.0b2"
 
 export CPATH="/opt/pyca/cryptography/openssl/include"
-export LIBRARY_PATH="/opt/pyca/cryptography/openssl/lib:/usr/lib:/usr/lib64:/lib:/lib64"
-
-yum install -y cmake
+export LIBRARY_PATH="/opt/pyca/cryptography/openssl/lib"
 
 # Build libuuid
 pushd /tmp
@@ -21,32 +21,36 @@ make
 make install
 popd
 
-cd /tmp
-mkdir azure-uamqp-c
-cd azure-uamqp-c
+mkdir /data/cmakebuild
+pushd /data/cmakebuild
 cmake28 /data/src/vendor/azure-uamqp-c \
     -Duse_openssl:bool=ON \
     -Duse_default_uuid:bool=ON \
+    -Dskip_samples:bool=ON \
     -DOPENSSL_LIBRARIES=/opt/pyca/cryptography/openssl/lib \
     -DOPENSSL_INCLUDE_DIR=/opt/pyca/cryptography/openssl/include \
     -DOPENSSL_ROOT_DIR=/opt/pyca/cryptography/openssl \
-    -DOPENSSL_SSL_LIBRARY=/opt/pyca/cryptography/openssl/lib/libssl.a \
-    -DOPENSSL_CRYPTO_LIBRARY=/opt/pyca/cryptography/openssl/lib/libcrypto.a \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
-    -DBUILD_MODE=static
+    -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE
+#    -DOPENSSL_SSL_LIBRARY=/opt/pyca/cryptography/openssl/lib/libssl.a \
+#    -DOPENSSL_CRYPTO_LIBRARY=/opt/pyca/cryptography/openssl/lib/libcrypto.a \
+#    -DBUILD_MODE=static
+
+cmake28 --build .
+popd
 
 # Make sure Cython and Wheel are available in all env
-#/opt/python/cp34-cp34m/bin/python -m pip install cython==0.27.3 wheel
-#/opt/python/cp35-cp35m/bin/python -m pip install cython==0.27.3 wheel
+/opt/python/cp34-cp34m/bin/python -m pip install cython==0.27.3 wheel
+/opt/python/cp35-cp35m/bin/python -m pip install cython==0.27.3 wheel
 /opt/python/cp36-cp36m/bin/python -m pip install cython==0.27.3 wheel
 
 # Build the wheels
 pushd /data
-#/opt/python/cp34-cp34m/bin/python setup.py bdist_wheel
-#auditwheel repair dist/uamqp-${UAMQP_VERSION}-cp34-cp34m-linux_x86_64.whl
+/opt/python/cp34-cp34m/bin/python setup.py bdist_wheel
+auditwheel repair dist/uamqp-${UAMQP_VERSION}-cp34-cp34m-linux_x86_64.whl
 
-#/opt/python/cp35-cp35m/bin/python setup.py bdist_wheel
-#auditwheel repair dist/uamqp-${UAMQP_VERSION}-cp35-cp35m-linux_x86_64.whl
+/opt/python/cp35-cp35m/bin/python setup.py bdist_wheel
+auditwheel repair dist/uamqp-${UAMQP_VERSION}-cp35-cp35m-linux_x86_64.whl
 
 /opt/python/cp36-cp36m/bin/python setup.py bdist_wheel
 auditwheel repair dist/uamqp-${UAMQP_VERSION}-cp36-cp36m-linux_x86_64.whl
+popd
